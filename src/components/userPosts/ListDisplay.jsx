3e7194ad765abab4b321632a99af5990a0ad9689
Jessica Swenson <jessicamswens@gmail.com>
2022-11-21 22:00:28 -0700

    started filter

--- src/components/userPosts/ListDisplay.jsx
+++ src/components/userPosts/ListDisplay.jsx
@@ -1,13 +1,20 @@
 import axios from 'axios';
 import React from 'react';
 import { useState } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import ListItem from './ListItem';
 import './ListDisplay.css';
 
 
 const ListDisplay = () => {
-    const [entries, setEntries] = useState([])
+    const [entries, setEntries] = useState([]);
+    const [filterValue, setFilterValue] = useState('All');
+
+    const allRef = useRef();
+    const fourByFourTrueRef = useRef();
+    const fourByFourFalseRef = useRef();
+    const dogFriendlyTrueRef = useRef();
+    const dogFriendlyFalseRef = useRef();
 
     const currentId = localStorage.getItem('user_id');
 
@@ -25,16 +32,46 @@ const ListDisplay = () => {
             .catch(err => console.log(err))
     };
 
+    const clickFilterHandler = () => {
+        setFilterValue(
+
+        )
+    }
+
+    const handleFilter = (entries) => {
+        if (filterValue === 'All') {
+            return entries
+        } else if (filterValue === 'fourByFourTrue') {
+            return entries.camp_entry_four_wheel === true;
+        }
+    }
+
     useEffect(() => {
         getList()
     }, [])
 
 
     return (
-        <div className='list-display-flex'>
-            {entries.map((entry, index) => {
-                return <ListItem entry={entry} />
-            })}
+        <div className='list-display-container'>
+            <div className='filter-div'>
+                <p>Filter By: </p>
+                <select>
+                    <option ref={allRef} value='All'>All</option>
+                    <option ref={fourByFourTrueRef} value='fourByFourTrue'>4x4 Required</option>
+                    <option ref={fourByFourFalseRef} value='fourByFourFalse'>4x4 Not Required</option>
+                    <option ref={dogFriendlyTrueRef} value='dogFriendlyTrue'>Dog Friendly</option>
+                    <option ref={dogFriendlyFalseRef} value='dogFriendlyFalse'>Not Dog Friendly</option>
+                </select>
+                <button onClick={clickFilterHandler} className='filter-btn' type='submit'>Apply</button>
+            </div>
+            <div className='list-display-flex'>
+                {entries
+                    .filter(handleFilter)
+
+                    .map((entry, index) => {
+                        return <ListItem entry={entry} />
+                    })}
+            </div>
 
         </div>
     )
